fc3248b67bdca71c65c7995561d806958775ef3f
ethicalhackerarbaz <95150363+ethicalhackerarbaz@users.noreply.github.com>
2021-11-28 05:12:11 +0530

    Update cast2video.sh
    Full hd

--- .myshfunc/cast2video.sh
+++ .myshfunc/cast2video.sh
@@ -4,5 +4,6 @@ cast2video() {
 echo -e "input: $1"
 ttygif --input $1 --output cast2video.gif --fps=33
 ffmpeg -f gif -i cast2video.gif -pix_fmt yuv420p -c:v libx264 -movflags +faststart -filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2' cast2video.mp4
-echo -e "output: cast2video.mp4"
+echo -e "output: cast2video.mkv
+"
 }
